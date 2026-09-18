36bfe193da4e8a7f86dfd23d1784ffa94333a0d6
Deepika R <deepikar128712@gmail.com>
2026-09-18 15:27:33 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const Inventory = require("./models/Inventory");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -94,7 +94,7 @@ mongoose
   .then(() => {
     console.log("MongoDB Connected Successfully!");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
   })
